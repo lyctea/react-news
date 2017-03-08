ba292b9e8d3a4f19c8107b603e23fc889758a0c1
lyctea <396136337@qq.com>
2017-03-08 22:41:55 +0800

    pc端首页

--- src/js/components/pc_newscontainer.js
+++ src/js/components/pc_newscontainer.js
@@ -3,6 +3,7 @@ import {Row, Col} from 'antd';
 import {Tabs, Carousel} from 'antd';
 const TabPane = Tabs.TabPane;
 import PCNewsBlock from './pc_news_block';
+import PCNewsImageBlock from './pc_news_image_block';
 
 export default class PCNewsContainer extends React.Component {
     render() {
@@ -25,13 +26,21 @@ export default class PCNewsContainer extends React.Component {
                                 <div><img src='./src/images/carousel_3.jpg'/></div>
                                 <div><img src='./src/images/carousel_4.jpg'/></div>
                             </Carousel>
+                            <PCNewsImageBlock count={6} type='guoji'  width="400px" cartTitle="国际头条" imageWidth='112px'/>
                         </div>
                     </div>
                     <Tabs class="tabs_news">
-                      <TabPane tab='新闻' key='1'>
+                      <TabPane tab='头条新闻' key='1'>
                         <PCNewsBlock count={22} type="top" width="100%" bordered="false"/>
                       </TabPane>
+                      <TabPane tab='国际' key='2'>
+                        <PCNewsBlock count={22} type="guoji" width="100%" bordered="false"/>
+                      </TabPane>
                     </Tabs>
+                    <div>
+                      <PCNewsImageBlock count={8} type='guonei'  width="100%" cartTitle="国内新闻" imageWidth='132px'/>
+                      <PCNewsImageBlock count={20} type='yule'  width="100%" cartTitle="娱乐新闻" imageWidth='132px'/>
+                    </div>
                 </Col>
                 <Col span={2}></Col>
             </div>
