02c92516da9833253b700e4fe80c1eca4f74962a
lyctea <396136337@qq.com>
2017-03-09 23:13:17 +0800

    pc端新闻详情

--- src/js/components/pc_news_block.js
+++ src/js/components/pc_news_block.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Card} from 'antd';
-import {Router, Route, Link, browserHistory} from 'react-router';
+import {Link} from 'react-router';
 
 export default class PCNewsBlock extends React.Component {
     constructor() {
@@ -19,26 +19,29 @@ export default class PCNewsBlock extends React.Component {
         .then(response => response.json())
         .then(json => this.setState({news: json}));
     };
+    dianji(){
+    	console.log('我被点击了');
+	};
 
     render() {
-
-      const {news} = this.state;
-      const newsList = news.length
-      ?
-      news.map((newsItem,index)=>(
-        <li key={index}>
-          <Link to={`details/${newsItem.uniquekey}`} target='_blank'>
-            {newsItem.title}
-          </Link>
-        </li>
-      ))
-      : '没有加载任何新闻' ;
-      return (
-            <div class='topNewsList'>
-                <Card>
-                    <ul>{newsList}</ul>
-                </Card>
-            </div>
-        )
-    }
+		const {news} = this.state;
+		const newsList = news.length
+			? news.map((newsItem, index) => (
+				<li key={index}>
+					<Link to={`details/${newsItem.uniquekey}`}  target="_blank">
+						{newsItem.title}
+					</Link>
+				</li>
+			))
+			: '没有加载到任何新闻';
+		return (
+			<div class="topNewsList">
+				<Card>
+					<ul>
+						{newsList}
+					</ul>
+				</Card>
+			</div>
+		);
+	};
 }
