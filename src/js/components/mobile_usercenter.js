98ac87f67cf7c0fc6820beb83750dc1333bebfbb
lyctea <396136337@qq.com>
2017-03-11 00:04:28 +0800

    头像模块

--- src/js/components/mobile_usercenter.js
+++ src/js/components/mobile_usercenter.js
@@ -5,11 +5,40 @@ import MobileFooter from './mobile_footer';
 import {
     Tabs,
     CheckBox,
+    Upload,
+    Icon,
+    Modal
 } from 'antd';
 const TabPane = Tabs.TabPane;
 
 export default class MobileUserCenter extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            previewImage: '',
+            previewVisible: false
+        };
+    };
     render() {
+        const props = {
+            action: 'http://newsapi.gugujiankong.com/handler.ashx',
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            listType: 'picture-card',
+            defaultFileList: [
+                {
+                    uid: -1,
+                    name: 'xxx.png',
+                    state: 'done',
+                    url: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png',
+                    thumbUrl: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png'
+                }
+            ],
+            onPreview: (file) => {
+                this.setState({previewImage: file.url, previewVisible: true});
+            }
+        };
         return (
             <div>
                 <MobileHeader/>
@@ -21,6 +50,16 @@ export default class MobileUserCenter extends React.Component {
                             <TabPane tab="我的评论列表" key="2">
                             </TabPane>
                             <TabPane tab="头像设置" key="3">
+                                <div className="clearfix">
+                                    <Upload {...props}>
+                                        <Icon type="plus"/>
+                                        <div className="ant-upload-text">上传照片</div>
+                                    </Upload>
+                                    <Modal visible={this.state.previewVisible} footer={null}
+                                           onCancel={this.handleCancel}>
+                                        <img src={this.state.previewImage} alt="预览"/>
+                                    </Modal>
+                                </div>
                             </TabPane>
                         </Tabs>
                     </Col>
--- src/js/components/pc_usercenter.js
+++ src/js/components/pc_usercenter.js
@@ -5,11 +5,41 @@ import PCFooter from './pc_footer';
 import {
     Tabs,
     CheckBox,
+    Upload,
+    Icon,
+    Modal
 } from 'antd';
 const TabPane = Tabs.TabPane;
 
 export default class PCUserCenter extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            previewImage: '',
+            previewVisible: false
+        };
+    };
+
     render() {
+        const props = {
+            action: 'http://newsapi.gugujiankong.com/handler.ashx',
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            listType: 'picture-card',
+            defaultFileList: [
+                {
+                    uid: -1,
+                    name: 'xxx.png',
+                    state: 'done',
+                    url: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png',
+                    thumbUrl: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png'
+                }
+            ],
+            onPreview: (file) => {
+                this.setState({previewImage: file.url, previewVisible: true});
+            }
+        };
         return (
             <div>
                 <Row>
@@ -22,6 +52,16 @@ export default class PCUserCenter extends React.Component {
                             <TabPane tab="我的评论列表" key="2">
                             </TabPane>
                             <TabPane tab="头像设置" key="3">
+                                <div className="clearfix">
+                                    <Upload {...props}>
+                                        <Icon type="plus"/>
+                                        <div className="ant-upload-text">上传照片</div>
+                                    </Upload>
+                                    <Modal visible={this.state.previewVisible} footer={null}
+                                           onCancel={this.handleCancel}>
+                                        <img src={this.state.previewImage} alt="预览"/>
+                                    </Modal>
+                                </div>
                             </TabPane>
                         </Tabs>
                     </Col>
