0d40e04e9008bc2207b7aa6532a479dddc026dd2
lyctea <396136337@qq.com>
2017-03-10 11:07:32 +0800

    pc端评论功能模块

--- src/js/components/pc_news_details.js
+++ src/js/components/pc_news_details.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Row, Col, BackTop} from 'antd';
 import PCHeader from './pc_header';
 import PCFooter from './pc_footer';
-import PCNewsImageBlock from './pc_news_image_block'
+import PCNewsImageBlock from './pc_news_image_block';
+import CommonComments from './common_comments';
 
 export default class PCNewsDetails extends React.Component {
     constructor() {
@@ -33,6 +34,8 @@ export default class PCNewsDetails extends React.Component {
                     <Col span={2}></Col>
                     <Col span={14} className='container'>
                         <div className='articleContainer' dangerouslySetInnerHTML={this.createMarkup()}></div>
+                        <hr/>
+                        <CommonComments uniquekey={this.props.params.uniquekey}/>
                     </Col>
                     <Col span={6}>
                         <PCNewsImageBlock count={40} type="top" width="100%" cardTitle="相关新闻" imageWidth="150px"/>
